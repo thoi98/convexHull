218ff3223792eda0db83dd792d32de7447a9d1bb
Thoi <thoithoi98@gmail.com>
2022-11-05 23:28:28 +0530

    init

---
+++ convexHull.py
@@ -0,0 +1,304 @@
+from ast import List
+import sys
+from tokenize import Double
+from typing import Tuple
+import pygame
+from pygame.locals import*
+from math import sqrt, sin, acos, cos
+import random
+
+width = 1000
+height = 500
+screen_color = (200, 200, 200)
+line_color = (255, 0, 0)
+
+
+def transFormCoords(coord):
+    return (width/2 + coord[0], height/2 - coord[1])
+
+
+def drawAxes(screen):
+    color = (100, 100, 100)
+    pygame.draw.line(screen, color, transFormCoords(
+        (0, -height/2)), transFormCoords((0, height/2)))
+    pygame.draw.line(screen, color, transFormCoords(
+        (-width/2, 0)), transFormCoords((width/2, 0)))
+
+
+def drawNewXAxis(screen, A, B):
+    a = B[1] - A[1]
+    b = A[0] - B[0]
+    # c = a*(A[0]) + b1(A[1])
+
+    p1: Tuple
+    p2: Tuple
+    p3: Tuple
+    p4: Tuple
+    if a > b:
+        p1 = (-1*(b*height)/(2*a), height/2)
+        p2 = ((b*height)/(2*a), -height/2)
+
+        p3 = ((a*height)/(2*b), height/2)
+        p4 = (-1*(a*height)/(2*b), -height/2)
+    else:
+        p1 = (width/2, -1*(a*width)/(2*b))
+        p2 = (-width/2, (a*width)/(2*b))
+
+        p3 = (width/2, (b*width)/(2*a))
+        p4 = (-width/2, -1*(b*width)/(2*a))
+    pygame.draw.line(screen, (250, 0, 0), transFormCoords(p1),
+                     transFormCoords(p2))
+    pygame.draw.line(screen, (0, 250, 0), transFormCoords(p3),
+                     transFormCoords(p4))
+
+
+def drawInfLine(screen, color, A, B, origin=False):
+    a = B[1] - A[1]
+    b = A[0] - B[0]
+    c = a*(A[0]) + b*(A[1])
+
+    if origin:
+        c = 0
+
+    p1: Tuple
+    p2: Tuple
+    if a > b:
+        p1 = (c - ((b*height)/(2*a)), height/2) if a != 0 else (c, height/2)
+        p2 = (c + ((b*height)/(2*a)), -height/2) if a != 0 else (c, -height/2)
+    else:
+        p1 = (width/2, c - ((a*width)/(2*b))) if b != 0 else (width/2, c)
+        p2 = (-width/2, c + ((a*width)/(2*b))) if b != 0 else (-width/2, c)
+    pygame.draw.line(screen, color, transFormCoords(p1),
+                     transFormCoords(p2))
+
+
+def drawPolygon(screen, points, color, isClosed=False):
+
+    size = len(points)
+    for i in range(1, size):
+        pygame.draw.line(screen, color, transFormCoords(
+            points[i-1]), transFormCoords(points[i]))
+    if isClosed == True:
+        pygame.draw.line(screen, color, transFormCoords(
+            points[0]), transFormCoords(points[size-1]))
+
+
+def getPerpendicularLine(A, B):
+    a = B[1] - A[1]
+    b = A[0] - B[0]
+    a_ = -b
+    b_ = a
+
+    c = a_*(A[0]) + b_*(A[1])
+
+    A_ = (0, c/b_) if b_ != 0 else (c/a_, 0)
+    B_ = (1, (c-a_)/(b_)) if b_ != 0 else (c/a_, 1)
+    return (A_, B_)
+
+
+def plotPoints(screen, points, color, radius=1):
+    for point in points:
+        pygame.draw.circle(screen, color, transFormCoords(point), radius)
+
+
+def getTilt(A, B):
+    a = B[1] - A[1]
+    b = A[0] - B[0]
+
+    if(a > 0 and b > 0) or (a < 0 and b < 0):
+        return -1
+    elif (a > 0 and b < 0) or (a < 0 and b > 0):
+        return 1
+    else:
+        return 0
+
+
+def convertCoords(point, A, B):
+    x1 = A[0]
+    y1 = A[1]
+    x2 = B[0]
+    y2 = B[1]
+
+    dx21 = x2-x1
+    dy21 = y2-y1
+
+    dx34 = 1
+    dy34 = 0
+    m12 = sqrt(dx21*dx21 + dy21*dy21)
+    m13 = sqrt(dx34*dx34 + dy34*dy34)
+    theta = acos((dx21*dx34 + dy21*dy34) / (m12 * m13))
+    # print("theta :"+str(theta*57.2958))
+    tilt = getTilt(A, B)
+    x_: Double
+    y_: Double
+    if tilt == -1:
+        x_ = point[0]*cos(theta)-point[1]*sin(theta)
+        y_ = point[1]*cos(theta)+point[0]*sin(theta)
+    else:  # elif tilt == 1:
+        x_ = point[0]*cos(theta)+point[1]*sin(theta)
+        y_ = -point[1]*cos(theta)+point[0]*sin(theta)
+    return (x_, y_)
+
+
+def convertCoordsList(points, A, B):
+    # a = B[1] - A[1]
+    # b = A[0] - B[0]
+    x1 = A[0]
+    y1 = A[1]
+    x2 = B[0]
+    y2 = B[1]
+
+    dx21 = x2-x1
+    dy21 = y2-y1
+
+    dx34 = 1
+    dy34 = 0
+    m12 = sqrt(dx21*dx21 + dy21*dy21)
+    m13 = sqrt(dx34*dx34 + dy34*dy34)
+    theta = acos((dx21*dx34 + dy21*dy34) / (m12 * m13))
+    print("theta :"+str(theta*57.2958))
+    result = []
+    tilt = getTilt(A, B)
+    for point in points:
+        x_: Double
+        y_: Double
+        if tilt == -1:
+            x_ = point[0]*cos(theta)-point[1]*sin(theta)
+            y_ = point[1]*cos(theta)+point[0]*sin(theta)
+        else:  # elif tilt == 1:
+            x_ = point[0]*cos(theta)+point[1]*sin(theta)
+            y_ = -point[1]*cos(theta)+point[0]*sin(theta)
+        result.append((x_, y_))
+    return result
+
+
+def getRandomPoints(x, factor=0.5):
+    points = []
+    for i in range(x):
+        x = random.randrange(-width/2, width/2)*factor
+        y = random.randrange(-height/2, height/2)*factor
+        points.append((x, y))
+    return points
+
+
+def getLeftRightBounds(points, A, B):
+    (A_, B_) = getPerpendicularLine(A, B)
+
+    def convert(point):
+        return convertCoords(point, A_, B_)
+
+    xleft = (points[0], points[0])
+    xright = (points[0], points[0])
+
+    for i in range(1, len(points)):
+        if convert(xleft[0])[0] > convert(points[i])[0]:
+            xleft = (points[i], points[i])
+        elif convert(xleft[0])[0] == convert(points[i])[0]:
+            if convert(xleft[1])[1] < convert(points[i])[1]:
+                xleft = (xleft[0], points[i])
+            elif convert(xleft[0])[1] > convert(points[i])[1]:
+                xleft = (points[i], xleft[1])
+
+        if convert(xright[0])[0] < convert(points[i])[0]:
+            xright = (points[i], points[i])
+        elif convert(xright[0])[0] == convert(points[i])[0]:
+            if convert(xright[1])[1] < convert(points[i])[1]:
+                xright = (xright[0], points[i])
+            elif convert(xright[0])[1] > convert(points[i])[1]:
+                xright = (points[i], xright[1])
+    # if A == xleft or B == xright or A == xright or B == xleft:
+    #     return (A, B)
+    return (xleft, xright)
+
+
+memo = dict()
+
+
+def rubberBand(points, A, B, direction):
+
+    if A[1] == B[1]:    # doing this because sometimes rotation gives slightly different values
+        direction = 1
+    if A == B:
+        return []
+    next: Tuple
+    if direction == -1:  # meaning _|_ of this line is tilting left
+        (next, _) = getLeftRightBounds(points, A, B)
+    else:
+        (_, next) = getLeftRightBounds(points, A, B)
+
+    if next[0] == A or next[0] == B or next[1] == A or next[1] == B:
+        return []
+    left = []
+    right = []
+    if direction == -1:
+        left = rubberBand(points, A, next[0], direction)
+        right = rubberBand(points, next[1], B, direction)
+    else:
+        left = rubberBand(points, A, next[1], direction)
+        right = rubberBand(points, next[0], B, direction)
+    result = []
+    result.extend(left)
+    if next[0] == next[1]:
+        result.extend([next[0]])
+    else:
+        if direction == -1:
+            result.extend([next[0], next[1]])
+        else:
+            result.extend([next[1], next[0]])
+    result.extend(right)
+    return result
+
+
+def solve(screen, points):
+    (xleft, xright) = getLeftRightBounds(points, (0, 0), (0, 1))
+    (ydown, yup) = getLeftRightBounds(points, (0, 0), (1, 0))
+
+    up_left = [xleft[1]]
+    up_left.extend(rubberBand(points, xleft[1], yup[0], -1))
+    up_left.extend([yup[0]])
+
+    down_left = [xleft[0]]
+    down_left.extend(rubberBand(points, xleft[0], ydown[0], -1))
+    down_left.extend([ydown[0]])
+
+    up_right = [yup[1]]
+    up_right.extend(rubberBand(points, yup[1], xright[1], 1))
+    up_right.extend([xright[1]])
+
+    down_right = [ydown[1]]
+    down_right.extend(rubberBand(points, ydown[1], xright[0], 1))
+    down_right.extend([xright[0]])
+
+    hull = []
+    hull.extend(up_left)
+    hull.extend(up_right)
+    hull.extend(list(reversed(down_right)))
+    hull.extend(list(reversed(down_left)))
+
+    plotPoints(screen, hull, (250, 0, 0), 2)
+
+    drawPolygon(screen, hull, (0, 250, 0), True)
+
+
+def main():
+    screen = pygame.display.set_mode((width, height))
+    screen.fill(screen_color)
+    drawAxes(screen)
+
+    # region test
+
+    points = getRandomPoints(20, 1/1.2)
+    print(points)
+    plotPoints(screen, points, (0, 0, 0), 1)
+    solve(screen, points)
+
+    # endregion
+    pygame.display.flip()
+
+    while True:
+        for events in pygame.event.get():
+            if events.type == QUIT:
+                sys.exit(0)
+
+
+main()
